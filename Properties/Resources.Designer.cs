3962b201f06baac2dfa263ca69a85907feac400d
Erick Josue Gutierrez Hernandez <erick_gutierrez@ucne.edu.do>
2019-05-28 19:42:43 -0400

    Consulta Valor inventario (cValorInventario)

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace EntregaParcialUno.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap reload_icon {
+            get {
+                object obj = ResourceManager.GetObject("reload-icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Save_icon {
             get {
                 object obj = ResourceManager.GetObject("Save-icon", resourceCulture);
