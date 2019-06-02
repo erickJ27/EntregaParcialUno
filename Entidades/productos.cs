579be76a3d5dfcded1f5431c87a694f8e87fb8e1
Erick Josue Gutierrez Hernandez <erick_gutierrez@ucne.edu.do>
2019-06-02 19:17:45 -0400

    Se creo la clase productos

--- Entidades/productos.cs
+++ Entidades/productos.cs
@@ -9,5 +9,22 @@ namespace EntregaParcialUno.Entidades
 {
     public class productos
     {
+        [Key]
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public decimal Costo { get; set; }
+        public decimal ValorInventario { get; set; }
+
+        public productos()
+        {
+            Id = 0;
+            Descripcion = string.Empty;
+            Existencia = 0;
+            Costo = 0;
+            ValorInventario = 0;
+        }
     }
+
+
 }
