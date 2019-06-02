a4e9d8f64b42faf0d9f067fc1bf996603b7a143e
Erick Josue Gutierrez Hernandez <erick_gutierrez@ucne.edu.do>
2019-06-02 19:39:54 -0400

    Se creo la clase productosBLL

---
+++ BLL/ProductosBLL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntregaParcialUno.DAL;
+using EntregaParcialUno.Entidades;
+using System.Data.Entity;
+using System.Linq.Expressions;
+
+namespace EntregaParcialUno.BLL
+{
+    public class ProductosBLL
+    {
+        public static bool Guardar(productos producto)
+        {
+            bool paso = false;
+
+            Contexto db = new Contexto();
+            try
+            {
+                if (db.Producto.Add(producto) != null)
+                    paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+            return paso;
+        }
+    }
+}
