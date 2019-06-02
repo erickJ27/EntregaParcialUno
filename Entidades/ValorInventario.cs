94286d7203727a50192d5fda993c6b4c10cecfe1
Erick Josue Gutierrez Hernandez <erick_gutierrez@ucne.edu.do>
2019-06-02 19:21:06 -0400

    Se creo la clase valor inventario

---
+++ Entidades/ValorInventario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntregaParcialUno.Entidades
+{
+    class ValorInventario
+    {
+        public decimal ValorTotalInventario { get; set; }
+
+        ValorInventario()
+        {
+            ValorTotalInventario = 0;
+        }
+    }
+}
