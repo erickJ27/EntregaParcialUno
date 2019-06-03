6e7c9dff8c2a6efec7bb3e8d914bff817443f371
Erick Josue Gutierrez Hernandez <erick_gutierrez@ucne.edu.do>
2019-06-02 22:10:50 -0400

    ValorInventarioBLL creado

---
+++ BLL/ValorInventarioBLL.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntregaParcialUno.DAL;
+using EntregaParcialUno.Entidades;
+using System.Data.Entity;
+using System.Linq.Expressions;
+
+namespace EntregaParcialUno.BLL
+{
+    public class ValorInventarioBLL
+    {
+        public static bool Guardar(ValorInventario valorinventario)
+        {
+            bool paso = false;
+
+            Contexto db = new Contexto();
+            try
+            {
+                if (db.ValorInventarioC.Add(valorinventario) != null)
+                    paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+            return paso;
+        }
+        public static List<ValorInventario> GetList(Expression<Func<ValorInventario, bool>> valorinventario)
+        {
+            List<ValorInventario> Lista = new List<ValorInventario>();
+            Contexto db = new Contexto();
+
+            try
+            {
+                Lista = db.ValorInventarioC.Where(valorinventario).ToList();
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return Lista;
+        }
+    }
+}
